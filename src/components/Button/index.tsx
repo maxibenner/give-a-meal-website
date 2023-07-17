215e5a493501e9a683020b8988b1c5a00c170061
Maximilian Benner <max@Maximilians-Air.fritz.box>
2023-07-17 15:43:49 +0200

    Update: Made Button className prop optional

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css"
 import Link from "next/link"
 
-export default function Button({ className, href, children }: { href: string, children: any, className: string }) {
+export default function Button({ className, href, children }: { href: string, children: any, className?: string }) {
     return <Link className={`${styles.container} body ${className}`} href={href}>{children}</Link>
 }
