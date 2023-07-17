481290f93dadbcfc4d75c121e12bc804e574430a
Maximilian Benner <maxibenner@gmail.com>
2023-07-17 21:57:12 +0200

    Update: Mobile landing optimizations

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -9,11 +9,21 @@ export default function Navigation() {
     <input type="checkbox" id="nav-button" className={styles.navButton} />
     <label htmlFor="nav-button" className={styles.navLabel}><p>&#9776;</p></label>
 
+
     <div className={styles.menuContainer}>
       <Link href="/" className="body">Get a Meal</Link>
       <Link href="/" className="body">Give a Meal</Link>
       <Link href="/" className="body">Restaurant Partners</Link>
       <Button href="/">Get the App</Button >
     </div>
+
+    <div className={styles.menuContainerMobile}>
+      <Link href="/" className="body">Get a Meal</Link>
+      <Link href="/" className="body">Give a Meal</Link>
+      <Link href="/" className="body">Restaurant Partners</Link>
+      <Button href="/">Get the App</Button >
+    </div>
+
+
   </nav>;
 }
