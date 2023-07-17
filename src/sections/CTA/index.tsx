8f43132baf336ceda7820dde25507db77f0028df
Maximilian Benner <max@Maximilians-Air.fritz.box>
2023-07-17 15:56:10 +0200

    Update: Marquee fix

--- src/sections/CTA/index.tsx
+++ src/sections/CTA/index.tsx
@@ -3,6 +3,7 @@
 import Button from "@/components/Button"
 import styles from "./styles.module.css"
 import RecentItem from "./RecentItem"
+import Marquee from "react-fast-marquee";
 
 export default function CTA() {
     const data = [
@@ -33,43 +34,7 @@ export default function CTA() {
             time: "1 hours ago",
             donorName: "",
             item: ""
-        },
-        {
-            action: "newBusiness",
-            businessName: "Mamalehh's Delicatessen",
-            time: "1 hours ago",
-            donorName: "",
-            item: ""
-        },
-        {
-            action: "newBusiness",
-            businessName: "Mamalehh's Delicatessen",
-            time: "1 hours ago",
-            donorName: "",
-            item: ""
-        },
-        {
-            action: "newBusiness",
-            businessName: "Mamalehh's Delicatessen",
-            time: "1 hours ago",
-            donorName: "",
-            item: ""
-        },
-        {
-            action: "newBusiness",
-            businessName: "Mamalehh's Delicatessen",
-            time: "1 hours ago",
-            donorName: "",
-            item: ""
-        },
-        {
-            action: "newBusiness",
-            businessName: "Mamalehh's Delicatessen",
-            time: "1 hours ago",
-            donorName: "",
-            item: ""
-        },
-
+        }
     ] as {
         action: "newDonation" | "newBusiness",
         businessName: string,
@@ -90,10 +55,9 @@ export default function CTA() {
                     </div>
                 </div>
                 <div className={styles.recentItemsContainer}>
-                    <div className={styles.marqueeContainer}>
+                    <Marquee>
                         {data.map((data) => <RecentItem key={data.time} action={data.action} businessName={data.businessName} time={data.time} donorName={data.donorName} item={data.item} />)}
-                        {data.map((data) => <RecentItem key={data.time} action={data.action} businessName={data.businessName} time={data.time} donorName={data.donorName} item={data.item} />)}
-                    </div>
+                    </Marquee>
                 </div>
             </div>
         </div >
