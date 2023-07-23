99cb3805756a639b0e3fca9ccffbd41479a6f261
Maximilian Benner <maxibenner@gmail.com>
2023-07-23 19:09:48 +0200

    Fix: escaped characters

--- src/app/restaurant-partners/infoCards/index.tsx
+++ src/app/restaurant-partners/infoCards/index.tsx
@@ -11,7 +11,7 @@ export default function InfoCards({ className }: { className?: string }) {
         <div className={`${styles.container} ${className}`}>
             <InfoCard onPointerEnter={() => setActiveIndex(0)} active={activeIndex === 0} className={styles.cardOne} title="Register Your Restaurant" body="Ready to join our mission? Download the Give a Meal Business app and register your restaurant to get started." number="1" />
             <InfoCard onPointerEnter={() => setActiveIndex(1)} active={activeIndex === 1} className={styles.cardTwo} title="Accept Meal Donations" body="When customers donate a meal, add it to the Give a Meal list using the Give a Meal Business app." number="2" />
-            <InfoCard onPointerEnter={() => setActiveIndex(2)} active={activeIndex === 2} className={styles.cardThree} title="Serve Donated Meals" body="When someone arrives with their Give a Meal QR code, prepare the meal and hand it over. You've just made someone's day a little brighter!" number="3" />
+            <InfoCard onPointerEnter={() => setActiveIndex(2)} active={activeIndex === 2} className={styles.cardThree} title="Serve Donated Meals" body="When someone arrives with their Give a Meal QR code, prepare the meal and hand it over. You&apos;ve just made someone&apos;s day a little brighter!" number="3" />
         </div>
     )
 }
