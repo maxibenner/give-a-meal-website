107c42929b4701e16c776bdc0d9a3cbcb5df265c
Maximilian Benner <maxibenner@gmail.com>
2023-07-24 10:34:05 +0200

    Update: Removed badge header tag

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css"
 
 export default function Badge({ children, className }: { children: any, className?: string }) {
-    return <h3 className={`${styles.container} ${className}`}>{children}</h3>
+    return <div className={`${styles.container} ${className}`}>{children}</div>
 }
