6542f549c42983a80a886e712337b6ef541771f0
Maximilian Benner <maxibenner@gmail.com>
2023-07-24 10:31:03 +0200

    Update: Changed hierarchy

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css"
 
 export default function Badge({ children, className }: { children: any, className?: string }) {
-    return <h5 className={`${styles.container} ${className}`}>{children}</h5>
+    return <h3 className={`${styles.container} ${className}`}>{children}</h3>
 }
