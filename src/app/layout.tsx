7bba7044f8194ef1b51281bebeb0138f4fa577af
Maximilian Benner <maxibenner@gmail.com>
2023-08-31 15:52:34 -0400

    Update: Replaced gtag with GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,17 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RHC4VS4EDR" />
-      <Script id="google-analytics">
+      <Script id="google-tag-manager">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-RHC4VS4EDR');
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5XJTLZGM');
         `}
       </Script>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5XJTLZGM" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
         <Navigation />
         <main>{children}</main>
         <Footer />
