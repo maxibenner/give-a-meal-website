109346f944071df30531f18afd58a155b548303d
Maximilian Benner <maxibenner@gmail.com>
2023-09-29 13:55:19 -0400

    Update: fi

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ import { Locale } from "@/i18n-config";
 import styles from "./styles.module.css";
 import localeLink from "@/utils/localeLink";
 
-export default function Footer({ lang, style }: { lang: Locale, style: React.CSSProperties }) {
+export default function Footer({ lang, style }: { lang: Locale, style?: React.CSSProperties }) {
     return <div className={styles.footerBg}>
         <footer className={styles.footer} style={style}>
             <div>
