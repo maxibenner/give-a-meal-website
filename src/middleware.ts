bc759706a21ba566899d9bc789a30231174fa072
Maximilian Benner <maxibenner@gmail.com>
2023-10-05 20:09:32 -0400

    Update: Added sitemap and robots to middleware exceptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,5 +62,5 @@ export function middleware(request: NextRequest) {
 
 export const config = {
     // Matcher ignoring `/_next/` and `/api/`
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 }
