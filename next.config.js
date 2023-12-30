176846157938b086cc629f6937894cae5d8609b4
Maximilian Benner <maxibenner@gmail.com>
2023-12-30 13:36:38 +0100

    Test: Manual file type

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/apple-app-site-association',
+                headers: [
+                    {
+                        key: 'Content-Type',
+                        value: 'application/json',
+                    },
+                ],
+            },
+        ];
+    },
+}
 
 module.exports = nextConfig
