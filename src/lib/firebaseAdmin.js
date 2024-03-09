cf6163eb9af0b2eacb7d8276439942f1b91fc22b
Maximilian Benner <maxibenner@gmail.com>
2024-03-09 14:16:20 -0500

    fix: newline replacement for env variable

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -4,7 +4,7 @@ const firebaseAdminConfig = {
   credential: cert({
     projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
     clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 };
 
