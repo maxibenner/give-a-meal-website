b083256086f779e158cf4d64819e0611e8c2a6a5
Maximilian Benner <maxibenner@gmail.com>
2024-05-12 17:42:39 -0400

    fix: linting

--- src/components/infoCards/index.tsx
+++ src/components/infoCards/index.tsx
@@ -1,15 +1,15 @@
-'use client'
+// 'use client'
 
 import InfoCard from "@/components/infoCard"
 import React from "react"
 import styles from "./styles.module.css"
 
 export default function InfoCards({ items, className }: { items: { title: string, sub: string }[], className?: string }) {
-    const [activeIndex, setActiveIndex] = React.useState(0)
+    // const [activeIndex, setActiveIndex] = React.useState(0)
 
     return (
         <div className={`${styles.container} ${className}`}>
-            {items.map((item, index) => <InfoCard key={item.title} onPointerEnter={() => setActiveIndex(index)} active={activeIndex === index} className={styles.cardOne} title={item.title} sub={item.sub} number={index + 1} />)}
+            {items.map((item, index) => <InfoCard key={item.title} /*onPointerEnter={() => setActiveIndex(index)}*/ active={index === 0} className={styles.cardOne} title={item.title} sub={item.sub} number={index + 1} />)}
         </div>
     )
 }
