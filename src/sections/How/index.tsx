3db3ea0ce162aa5ee2c70af5fe06c711f3b30359
Maximilian Benner <maxibenner@gmail.com>
2024-05-12 15:56:48 -0400

    style: updates to infoCard

--- src/sections/How/index.tsx
+++ src/sections/How/index.tsx
@@ -7,20 +7,22 @@ import { Locale } from "@/i18n-config"
 import React, { useEffect } from "react"
 import styles from "./styles.module.css"
 
-export default function How({ lang }: { lang: Locale }) {
-    const [activeIndex, setActiveIndex] = React.useState(0)
-    const [dictionary, setDictionary] = React.useState<any>(null)
-    useEffect(() => {
-        getDictionary(lang).then((res) => setDictionary(res))
-    }, [])
+export default async function How({ lang }: { lang: Locale }) {
+    const { pages: { home: { howTo: { cards, title } } } } = await getDictionary(lang)
 
-    if (!dictionary) return null
+    // const [activeIndex, setActiveIndex] = React.useState(0)
+    // const [dictionary, setDictionary] = React.useState<any>(null)
+    // useEffect(() => {
+    //     getDictionary(lang).then((res) => setDictionary(res))
+    // }, [])
+
+    // if (!dictionary) return null
     return (
         <div className={`${styles.container} grid`}>
-            <Badge className={styles.badge}>{dictionary.pages.home.howTo.title}</Badge>
-            <InfoCard active className={styles.cardOne} title={dictionary.pages.home.howTo.cards[0].title} sub={dictionary.pages.home.howTo.cards[0].sub} number={1} />
-            <InfoCard className={styles.cardTwo} title={dictionary.pages.home.howTo.cards[1].title} sub={dictionary.pages.home.howTo.cards[1].sub} number={2} />
-            <InfoCard className={styles.cardThree} title={dictionary.pages.home.howTo.cards[2].title} sub={dictionary.pages.home.howTo.cards[2].sub} number={4} />
+            <Badge className={styles.badge}>{title}</Badge>
+            <InfoCard active className={styles.cardOne} title={cards[0].title} sub={cards[0].sub} number={1} />
+            <InfoCard className={styles.cardTwo} title={cards[1].title} sub={cards[1].sub} number={2} />
+            <InfoCard className={styles.cardThree} title={cards[2].title} sub={cards[2].sub} number={4} />
         </div>
     )
 }
