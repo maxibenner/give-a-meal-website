1490229373c44acb5b29692d9980a0741412338d
Maximilian Benner <maxibenner@gmail.com>
2024-05-26 16:55:28 -0400

    fix: font issues

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -6,13 +6,17 @@ import LocaleSwitcher from "../localeSwitcher/LocaleSwitcher";
 import NavigationDesktop from "./NavigationDesktop";
 import NavigationMobile from "./NavigationMobile";
 import styles from "./styles.module.css";
+import Logo from "@/public/assets/icons/logo.svg"
+import Image from "next/image";
 
 export default async function Navigation({ lang }: { lang: Locale }) {
 
   const { elements: { nav } } = await getDictionary(lang)
 
   return <nav className={styles.container}>
-    <Link className={styles.wordmark} href={localeLink("/", lang)}>Give a Meal</Link>
+    <Link className={styles.wordmark} href={localeLink("/", lang)}>
+      <Image src={Logo} alt="Give a Meal Logo" width={167} height={29} />
+    </Link>
 
     <div className={styles.menuWrapper}>
       <LocaleSwitcher className={styles.buttonBox} />
