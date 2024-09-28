c959ffb132eef65c578e3a49086cb7307618275f
Maximilian Benner <maxibenner@gmail.com>
2024-09-28 15:00:40 -0400

    feat: custom host

--- src/app/api/auth/verify-email-link/route.ts
+++ src/app/api/auth/verify-email-link/route.ts
@@ -14,20 +14,21 @@ initAdminApp();
 
 export async function GET(request: NextRequest) {
   let userCredential: UserCredential | undefined;
+  let url = `${process.env.NEXT_HOST}${request.nextUrl.pathname}${request.nextUrl.search}`;
 
   // Get email query parameters
   const email = request.nextUrl.searchParams.get("email");
   const lang = request.nextUrl.searchParams.get("user-lang");
 
-  const isValid = isSignInWithEmailLink(authConfig, request.url);
-  console.log("Email URl: ", request.url);
+  const isValid = isSignInWithEmailLink(authConfig, url);
+  console.log("Email URl: ", url);
   console.log("Is valid: ", isValid);
 
   if (email && isValid) {
-    userCredential = await signInWithEmailLink(authConfig, email, request.url);
+    userCredential = await signInWithEmailLink(authConfig, email, url);
     console.log("User credential: ", userCredential);
   } else {
-    NextResponse.redirect(request.url + "/donors/login");
+    NextResponse.redirect(url + "/donors/login");
   }
 
   if (userCredential) {
